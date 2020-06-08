78d3cf9ff0d771ce89ec27f4f352c053bd327153
ZhivkoB <zikazaco@gmail.com>
2020-06-08 11:19:23 -0500

    update container background color

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`
+    title: `PlacerWeb Portfolio`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
